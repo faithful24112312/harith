e4272ee13e099f2753a82d1b3726ed83d857a9e8
faithful24112312 <85530392+faithful24112312@users.noreply.github.com>
2021-07-02 17:41:50 +0700

    hah

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://faithful24112312.github.io/harith/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"dac5854989"};
+var userProfileL10n = {"user_id":"0","nonce":"3829df1d7f"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
